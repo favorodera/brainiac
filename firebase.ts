24bd6db6a182c4de93d3afefba99812128ac8e31
Favour Chidera Emeka <favorodera@gmail.com>
2024-07-25 15:32:40 +0100

    added firebase

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+const runtimeConfig = useRuntimeConfig();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: runtimeConfig.firebaseApiKey,
   authDomain: "brainiacaichat.firebaseapp.com",
   projectId: "brainiacaichat",
   storageBucket: "brainiacaichat.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: runtimeConfig.firebaseMessagingSenderId,
+  appId: runtimeConfig.firebaseAppId,
+  measurementId: runtimeConfig.firebaseMeasurementId,
 };
 
 // Initialize Firebase
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,4 +21,7 @@ export default defineNuxtConfig({
       ],
     },
   },
+  nitro: {
+    preset: "firebase",
+  },
 });
